9dae98f56f1862f9441cb6d376451696065fc728
Julian Villegas <julian.villegasplus@gmail.com>
2020-02-14 06:23:27 +0000

    [ADD]base_model.py: class BaseModel that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Base module """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """ class for all other classes to inherit from """
+    def __init__(self, id='', created_at='', updated_at=''):
+        """ Constructor """
+        # Generate a random UUID
+        self.id = str(uuid.uuid4())
+        # assign with the current datetime when an instance is created
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ overriding the __str__ method that returns a custom
+        string object """
+        # Old-style: self.__class__.__name__
+        class_name = type(self).__name__
+        mssg = "[{0}] ({1}) {2}".format(class_name, self.id, self.__dict__)
+        return (mssg)
+
+    # Public instance methods
+    def save(self):
+        """ updates the public instance attribute updated_at with
+        the current datetime """
+        self.updated_at = datetime.now()
