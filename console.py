7767c570a9220d6eb1cd80a06347bfab035b26c6
g5sh <eagomez2387@gmail.com>
2020-02-14 15:08:18 -0500

    [ADD]: Add console.py 1.0

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """init Command Prompt"""
+    prompt = "(hbnb)"
+
+    def do_EOF(self, args):
+        """CTRl-D to exit"""
+        print()
+        return True
+
+    def do_quit(self, args):
+        """exit the program"""
+        return True
+    def empty_line(self):
+        """ENTER shouldn’t execute anything"""
+        pass
