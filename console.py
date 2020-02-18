68d32d64459fdeebd0c78a4a2a8c81a835881476
g5sh <eagomez2387@gmail.com>
2020-02-18 11:11:31 -0500

    [ADD]: Add funtions a console.py

--- console.py
+++ console.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ Program that contains the entry point of the command interpreter """
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """ init Command Prompt """
     prompt = "(hbnb) "
+    lavel =  {"BaseModel", "City", "State"
+              "User", "Place", "Review", "Ameity"}
 
     def do_EOF(self, args):
         """CTRl-D to exit\n"""
@@ -21,34 +23,47 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, line):
-        """Create a instance by the user"""
+        """Create a instance of BaseModel"""
         if len(line) == 0:
-            print ("** class name missing **")
-        elif line not in HBNBCommand.classes:
-            print("** class doesn't exist **")
+            print("** class name missing **")
+            return None
+        elif line not in HBNBCommand.lavel:
+            print("** class name missing **")
+            return None
         else:
-            inst = eval(line)()
-            inst.save()
-            print (inst.id)
+            my_inst = eval(line + "()")
+            my_inst.save()
+            print my_inst.id
 
     def do_show(self, line):
+        """Prints the string representation of
+        an instance based on the class name and id"""
         if len(line) == 0:
-            print ("** class name missing **")
-            return
-        ar = parse(line)
-
-        if ar[0] is not HBNBCommand.classes:
-            print ("** class doesn't exist **")
-            return
+            print("** class name missing **")
+            return None
+        n = parse(line)
+        if ar[0] not in HBNBCommand.lavel:
+            print("** class doesn't exist **")
+            return None
         try:
-            if ar[1]:
-                n = "{}.{}".format(ar[0], ar[1])
-                if n is not in storage.all().key():
-                    print ("** no instance found **")
+            if n[1]:
+                ite = "{},{}".format(n[0], n[1])
+                if ite not in storage.all().keys():
+                    print("** no instance found **")
                 else:
-                    print storage.all()[n]
-        except IndexError:
+                    print(storage.all()[ite])
+        except:
             print("** instance id missing **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+
+    def do_all(self, line):
+        """Prints all string representation of
+        all instances based or not on the class name."""
+
+    def do_update(self, line)
+    """Updates an instance based on the class name and id"""
 
 
 if __name__ == '__main__':
