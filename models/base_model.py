5682a967cf82e3839de8ee0c1e890a06956e07e0
Julian Villegas <julian.villegasplus@gmail.com>
2020-02-19 16:30:21 +0000

    [FIX]base_model: code fail in checkers

--- models/base_model.py
+++ models/base_model.py
@@ -21,23 +21,22 @@ class BaseModel:
                     # Convert string date to datetime object
                     # strptime (string parse time): Parse a string into a -
                     # datetime object given a corresponding format
-                    self.updated_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "created_at":
-                    self.created_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "__class__":
                     # This happens because __class__ is not mandatory in output
                     continue
 
                 setattr(self, key, value)
-        # Generate a random UUID
-        self.id = str(uuid.uuid4())
-        # assign with the current datetime when an instance is created
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        # if it’s a new instance add a call to the method new(self) on storage
-        models.storage.new(self)
+        else:
+            # Generate a random UUID
+            self.id = str(uuid.uuid4())
+            # assign with the current datetime when an instance is created
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            # if it’s a new instance add a call to the method new(self) on stge
+            models.storage.new(self)
 
     def __str__(self):
         """ overriding the __str__ method that returns a custom
