20e5dfcc95bb6663e28b47942667018d3a2424b6
g5sh <eagomez2387@gmail.com>
2020-02-19 11:56:18 -0500

    [TEST]: Add Unittests City

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -14,39 +14,42 @@ class test_Amenity(unittest.TestCase):
 
     @classmethod
     def setup(self):
-        self.Amenity = Amenity()
-        self.Amenity.name = "Yggdrasil"
+        self.city1 = City()
+        self.city1.name = "Zanarkand"
+        self.city1.state_id = "Spira"
 
     @classmethod
     def tearDown(self):
-        del self.Amenity
+        del self.city1
         try:
             os.remove("file.json")
         except FileNotFoundError:
             pass
 
     def test_to_dict(self):
-        self.assertEqual("to_dict" in dir(self.Amenity), True)
+        self.assertEqual("to_dict" in dir(self.city1), True)
 
     def test_functions(self):
-        self.assertIsNotNone(Amenity.__dict__)
+        self.assertIsNotNone(City.__dict__)
 
     def save_test(self):
-        self.Amenity.save()
-        self.assertNotEqual(self.Amenity.created_at,
-                            self.Amenity.updated_at)
+        self.city1.save()
+        self.assertNotEqual(self.city1.created_at,
+                            self.city1.updated_at)
 
     def test_subclass(self):
-        self.assertTrue(issubclass(self.Amenity.__class__.BaseModel), True)
+        self.assertTrue(issubclass(self.city1.__class__.BaseModel), True)
 
     def test_attributes(self):
-        self.assertTrue("name" in self.Amenity.__dict__)
-        self.assertTrue("created_at" in self.Amenity.__dict__)
-        self.assertTrue("updated_at" in self.Amenity.__dict__)
-        self.assertTrue("id" in self.Amenity.__dict__)
+        self.assertTrue("name" in self.city1.__dict__)
+        self.assertTrue("created_at" in self.city1.__dict__)
+        self.assertTrue("updated_at" in self.city1.__dict__)
+        self.assertTrue("id" in self.city1.__dict__)
+        self.assertTrue("state_id" in self.city1.__dict__)
 
     def test_strings(self):
-        self.assertEqual(type(self.Amenity.name), str)
+        self.assertEqual(type(self.city1.name), str)
+        self.assertEqual(type(self.city1.state_id), str)
 
 if __name__ = "__main__":
     unittest.main()
