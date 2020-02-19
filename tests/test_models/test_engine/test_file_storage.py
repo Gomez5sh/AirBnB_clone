80f5d3251c4eba5932f9f9d97ad554cf97f8375a
g5sh <eagomez2387@gmail.com>
2020-02-19 16:43:50 -0500

    [UNITTEST]: Add unittes file storge

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -61,3 +61,5 @@ class test_filestore(unittest.TestCase):
             for n in r:
                 self.assertEqual(n, "{}")
         self.assertIs(file3.reload(), None)
+if __name__ = "__main__":
+    unittest.main()
