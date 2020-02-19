1f109a08dcf4c00941827e85f8e57032f8112096
g5sh <eagomez2387@gmail.com>
2020-02-19 13:55:28 -0500

    [UNITTEST]: Add unittes and fix

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -13,39 +13,39 @@ from models.base_model import BaseModel
 class test_Amenity(unittest.TestCase):
         @classmethod
         def setup(self):
-            self.Amenity = Amenity()
-            self.Amenity.name = "Yggdrasil"
+            self.amenity1 = Amenity()
+            self.amenity1.name = "Yggdrasil"
 
         @classmethod
         def tearDown(self):
-            del self.Amenity
+            del self.amenity1
             try:
                 os.remove("file.json")
             except FileNotFoundError:
                 pass
 
         def test_to_dict(self):
-            self.assertEqual("to_dict" in dir(self.Amenity), True)
+            self.assertEqual("to_dict" in dir(self.amenity1), True)
 
         def test_functions(self):
-            self.assertIsNotNone(Amenity.__dict__)
+            self.assertIsNotNone(Amenity.__doc__)
 
         def save_test(self):
-            self.Amenity.save()
-            self.assertNotEqual(self.Amenity.created_at,
-                                self.Amenity.updated_at)
+            self.amenity1.save()
+            self.assertNotEqual(self.amenity1.created_at,
+                                self.amenity1.updated_at)
 
         def test_subclass(self):
-            self.assertTrue(issubclass(self.Amenity.__class__.BaseModel), True)
+            self.assertTrue(issubclass(self.amenity1.__class__.BaseModel), True)
 
         def test_attributes(self):
-            self.assertTrue("name" in self.Amenity.__dict__)
-            self.assertTrue("created_at" in self.Amenity.__dict__)
-            self.assertTrue("updated_at" in self.Amenity.__dict__)
-            self.assertTrue("id" in self.Amenity.__dict__)
+            self.assertTrue("name" in self.amenity1.__dict__)
+            self.assertTrue("created_at" in self.amenity1.__dict__)
+            self.assertTrue("updated_at" in self.amenity1.__dict__)
+            self.assertTrue("id" in self.amenity1.__dict__)
 
         def test_strings(self):
-            self.assertEqual(type(self.Amenity.name), str)
+            self.assertEqual(type(self.amenity1.name), str)
 
 if __name__ = "__main__":
     unittest.main()
